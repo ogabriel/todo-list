fb1984062b0f6fbdb38934f82a29bd4e10996740
ogabriel <gabrieloliver8991@gmail.com>
2024-01-18 08:57:26 -0300

    add base script.js

---
+++ script.js
@@ -0,0 +1,38 @@
+const btn = document.querySelector('#todo-btn')
+
+const deleteTodo = (e) => {
+    const todo = e.target.parentElement
+    todo.remove()
+}
+
+const checkTodo = (e) => {
+    const todo = e.target.parentElement
+    todo.classList.toggle('checked')
+}
+
+btn.addEventListener('click', () => {
+    const todo = document.querySelector('#todo-input').value
+
+    if (todo === '') {
+        alert('Please enter a todo')
+        return
+    }
+
+    const todoList = document.querySelector('#todo-list')
+    const newTodo = document.createElement('div')
+
+    const newTodoListItem = document.createElement('li')
+    newTodoListItem.innerText = todo
+    const newTodoDeleteBtn = document.createElement('button')
+    newTodoDeleteBtn.innerText = 'Delete'
+    newTodoDeleteBtn.addEventListener('click', deleteTodo)
+    const newTodoCheckBtn = document.createElement('button')
+    newTodoCheckBtn.innerText = 'Check'
+    newTodoCheckBtn.addEventListener('click', checkTodo)
+
+    newTodo.appendChild(newTodoListItem)
+    newTodo.appendChild(newTodoDeleteBtn)
+    newTodo.appendChild(newTodoCheckBtn)
+
+    todoList.appendChild(newTodo)
+})
