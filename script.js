958acdebb090ac465755661fbf7c8456c4bd3bab
ogabriel <gabrieloliver8991@gmail.com>
2024-02-17 21:42:37 -0300

    fix formatting

--- script.js
+++ script.js
@@ -1,134 +1,133 @@
 const deleteTodoEvent = (e) => {
-    const todoDiv = e.target.parentElement
-    deleteTodo(todoDiv)
-}
+  const todoDiv = e.target.parentElement;
+  deleteTodo(todoDiv);
+};
 
 const deleteTodo = (todoDiv) => {
-    const todoListItem = todoDiv.getElementsByTagName('li')[0]
-    const todo = todoListItem.innerText
+  const todoListItem = todoDiv.getElementsByTagName("li")[0];
+  const todo = todoListItem.innerText;
 
-    todoDiv.remove()
-    removeData(todo)
-}
+  todoDiv.remove();
+  removeData(todo);
+};
 
 const checkTodoEvent = (e) => {
-    const todoDiv = e.target.parentElement
-    checkTodo(todoDiv)
-}
+  const todoDiv = e.target.parentElement;
+  checkTodo(todoDiv);
+};
 
 const checkTodo = (todoDiv) => {
-    const todoListItem = todoDiv.getElementsByTagName('li')[0]
-    const todo = todoListItem.innerText
+  const todoListItem = todoDiv.getElementsByTagName("li")[0];
+  const todo = todoListItem.innerText;
 
-    const toggle = todoListItem.classList.toggle('checked')
+  const toggle = todoListItem.classList.toggle("checked");
 
-    if (toggle) {
-        saveData(todo, 'checked')
-    } else {
-        saveData(todo, 'unchecked')
-    }
-}
+  if (toggle) {
+    saveData(todo, "checked");
+  } else {
+    saveData(todo, "unchecked");
+  }
+};
 
 const editTodoEvent = (e) => {
-    const todoDiv = e.target.parentElement
-    editTodo(todoDiv)
-}
+  const todoDiv = e.target.parentElement;
+  editTodo(todoDiv);
+};
 
 const editTodo = (todoDiv) => {
-    const todoListItem = todoDiv.getElementsByTagName('li')[0]
-    const todo = todoListItem.innerText
+  const todoListItem = todoDiv.getElementsByTagName("li")[0];
+  const todo = todoListItem.innerText;
 
-    const newTodo = prompt('Edit task', todo)
+  const newTodo = prompt("Edit task", todo);
 
-    if (newTodo != todo) {
-        todoListItem.innerText = newTodo
-        removeData(todo)
+  if (newTodo != todo) {
+    todoListItem.innerText = newTodo;
+    removeData(todo);
 
-        if (todoListItem.classList.contains('checked')) {
-            saveData(newTodo, 'checked')
-        } else {
-            saveData(newTodo, 'unchecked')
-        }
+    if (todoListItem.classList.contains("checked")) {
+      saveData(newTodo, "checked");
+    } else {
+      saveData(newTodo, "unchecked");
     }
-
-}
+  }
+};
 
 const addTodo = (todo) => {
-    const todoList = document.querySelector('#todo-list')
+  const todoList = document.querySelector("#todo-list");
 
-    const newTodo = document.createElement('div')
-    newTodo.classList.add('todo')
+  const newTodo = document.createElement("div");
+  newTodo.classList.add("todo");
 
-    const newTodoListItem = document.createElement('li')
-    newTodoListItem.innerText = todo
+  const newTodoListItem = document.createElement("li");
+  newTodoListItem.innerText = todo;
 
-    const newTodoEditBtn = document.createElement('button')
-    newTodoEditBtn.innerText = 'Edit'
-    newTodoEditBtn.addEventListener('click', editTodoEvent)
+  const newTodoEditBtn = document.createElement("button");
+  newTodoEditBtn.innerText = "Edit";
+  newTodoEditBtn.addEventListener("click", editTodoEvent);
 
-    const newTodoDeleteBtn = document.createElement('button')
-    newTodoDeleteBtn.innerText = 'Delete'
-    newTodoDeleteBtn.addEventListener('click', deleteTodoEvent)
+  const newTodoDeleteBtn = document.createElement("button");
+  newTodoDeleteBtn.innerText = "Delete";
+  newTodoDeleteBtn.addEventListener("click", deleteTodoEvent);
 
-    const newTodoCheckBtn = document.createElement('button')
-    newTodoCheckBtn.innerText = 'Check'
-    newTodoCheckBtn.addEventListener('click', checkTodoEvent)
+  const newTodoCheckBtn = document.createElement("button");
+  newTodoCheckBtn.innerText = "Check";
+  newTodoCheckBtn.addEventListener("click", checkTodoEvent);
 
-    newTodo.appendChild(newTodoListItem)
-    newTodo.appendChild(newTodoEditBtn)
-    newTodo.appendChild(newTodoDeleteBtn)
-    newTodo.appendChild(newTodoCheckBtn)
+  newTodo.appendChild(newTodoListItem);
+  newTodo.appendChild(newTodoEditBtn);
+  newTodo.appendChild(newTodoDeleteBtn);
+  newTodo.appendChild(newTodoCheckBtn);
 
-    todoList.appendChild(newTodo)
+  todoList.appendChild(newTodo);
 
-    return newTodo
-}
+  return newTodo;
+};
 
 const validateTodo = (todo) => {
-    if (todo === '') {
-        alert('Please enter a task name')
-        return false
-    }
+  if (todo === "") {
+    alert("Please enter a task name");
+    return false;
+  }
 
-    const currentTodo = localStorage.getItem(todo)
+  const currentTodo = localStorage.getItem(todo);
 
-    if (currentTodo == null) {
-        alert('All tasks must be unique')
-        return false
-    }
-}
+  if (currentTodo == null) {
+    alert("All tasks must be unique");
+    return false;
+  }
+};
 
-const btn = document.querySelector('#todo-input-btn')
+const btn = document.querySelector("#todo-input-btn");
 
-btn.addEventListener('click', () => {
-    const todo = document.querySelector('#todo-input').value
+btn.addEventListener("click", () => {
+  const todo = document.querySelector("#todo-input").value;
 
-    if (!validateTodo()) {
-        return
-    }
+  if (!validateTodo()) {
+    return;
+  }
 
-    addTodo(todo)
-    saveData(todo, 'unchecked')
-})
+  addTodo(todo);
+  saveData(todo, "unchecked");
+});
 
 const saveData = (key, value) => {
-    localStorage.setItem(key, value)
-}
+  localStorage.setItem(key, value);
+};
 
 const removeData = (key) => {
-    localStorage.removeItem(key)
-}
+  localStorage.removeItem(key);
+};
 
 const loadData = () => {
-    for (let index = 0; index < localStorage.length; index++) {
-        const key = localStorage.key(index)
-        const value = localStorage.getItem(key)
-        const newTodo = addTodo(key);
+  for (let index = 0; index < localStorage.length; index++) {
+    const key = localStorage.key(index);
+    const value = localStorage.getItem(key);
+    const newTodo = addTodo(key);
 
-        if (value == 'checked') {
-            checkTodo(newTodo)
-        }
+    if (value == "checked") {
+      checkTodo(newTodo);
     }
-}
+  }
+};
 
-window.addEventListener('load', loadData)
+window.addEventListener("load", loadData);
