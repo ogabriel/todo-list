53054d48f1478aa6838a9240c9fe7b753bad742d
ogabriel <gabrieloliver8991@gmail.com>
2026-05-08 20:52:18 -0300

    add delete task

--- moz-todo-react/src/App.jsx
+++ moz-todo-react/src/App.jsx
@@ -25,6 +25,12 @@ function App(props) {
     setTasks(updatedTasks);
   }
 
+  function deleteTask(id) {
+    const updatedTasks = tasks.filter((task) => id !== task.id);
+
+    setTasks(updatedTasks);
+  }
+
   const taskList = tasks?.map((task) => (
     <Todo
       id={task.id}
@@ -32,6 +38,7 @@ function App(props) {
       completed={task.completed}
       key={task.id}
       toggleTaskCompleted={toggleTaskCompleted}
+      deleteTask={deleteTask}
     />
   ));
 
--- moz-todo-react/src/components/Todo.jsx
+++ moz-todo-react/src/components/Todo.jsx
@@ -7,6 +7,7 @@ function Todo(props) {
             id={props.id}
             type="checkbox"
             defaultChecked={props.completed}
+            onChange={() => props.toggleTaskCompleted(props.id)}
           />
           <label className="todo-label" htmlFor={props.id}>
             {props.name}
@@ -16,7 +17,11 @@ function Todo(props) {
           <button type="button" className="btn">
             Edit <span className="visually-hidden">{props.name}</span>
           </button>
-          <button type="button" className="btn btn__danger">
+          <button
+            type="button"
+            className="btn btn__danger"
+            onClick={() => props.deleteTask(props.id)}
+          >
             Delete <span className="visually-hidden">{props.name}</span>
           </button>
         </div>
