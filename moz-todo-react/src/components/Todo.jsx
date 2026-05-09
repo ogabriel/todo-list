d4e658d6ed06f7eda9571bc4709ef8ff75849da0
ogabriel <gabrieloliver8991@gmail.com>
2026-05-08 22:24:20 -0300

    add useEffect and custom hook to handle tab/enter behavior

--- moz-todo-react/src/components/Todo.jsx
+++ moz-todo-react/src/components/Todo.jsx
@@ -1,8 +1,21 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
+
+function usePrevious(value) {
+  const ref = useRef();
+  useEffect(() => {
+    ref.current = value;
+  });
+  return ref.current;
+}
 
 function Todo(props) {
   const [isEditing, setEditing] = useState(false);
   const [newName, setNewName] = useState("");
+
+  const editFieldRef = useRef(null);
+  const editButtonRef = useRef(null);
+
+  const wasEditing = usePrevious(isEditing);
 
   function handleChange(e) {
     setNewName(e.target.value);
@@ -14,6 +27,14 @@ function Todo(props) {
     setNewName("");
     setEditing(false);
   }
+
+  useEffect(() => {
+    if (!wasEditing && isEditing) {
+      editFieldRef.current.focus();
+    } else if (wasEditing && !isEditing) {
+      editButtonRef.current.focus();
+    }
+  }, [wasEditing, isEditing]);
 
   const editingTemplate = (
     <form className="stack-small" onSubmit={handleSubmit}>
@@ -27,6 +48,7 @@ function Todo(props) {
           type="text"
           value={newName}
           onChange={handleChange}
+          ref={editFieldRef}
         />
       </div>
       <div className="btn-group">
@@ -61,7 +83,12 @@ function Todo(props) {
         </label>
       </div>
       <div className="btn-group">
-        <button type="button" className="btn" onClick={() => setEditing(true)}>
+        <button
+          type="button"
+          className="btn"
+          onClick={() => setEditing(true)}
+          ref={editButtonRef}
+        >
           Edit <span className="visually-hidden">{props.name}</span>
         </button>
 
